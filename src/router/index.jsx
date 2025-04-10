5da9b3be4f75ef788ea293794ac8d8bfc5ecb1f8
Hyunjun Harry Yim <harryyim707@gmail.com>
2025-04-10 18:28:39 +0900

    ✨ feat: 불필요한 import 제거 및 Router 구성 간소화

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,17 +1,13 @@
-import { useMediaQuery, useTheme } from '@mui/material'
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import ResponsiveLayout from '../layouts/ResponsiveLayout'
-import Home from '../pages/Home'
 import PerformanceDetail from '../pages/PerformanceDetail'
 import Performance from '../pages/PerformanceList'
 
 function AppRouter() {
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'))
   const basename = '/momentum-web'
 
   return (
-    <Router basename={basename} future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
+    <Router basename={basename}>
       <Routes>
         <Route path='/' element={<Navigate to={'/performances/20250001'} />} />
         <Route
